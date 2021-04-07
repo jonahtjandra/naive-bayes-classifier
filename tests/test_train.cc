7a0410919217cd507a024736ef9fb9c1c4eef33d
jonah <jonahlt2@illinois.edu>
2021-04-07 11:11:04 +0700

    Implemented parsing of images with different sizes.

--- tests/test_train.cc
+++ tests/test_train.cc
@@ -23,3 +23,24 @@ TEST_CASE(">> operator overload") {
     }
 }
 
+TEST_CASE("Parsing different image sizes") {
+    naivebayes::Train train(5);
+    std::string file_path = "/Users/jonah/Desktop/SP2021/Cinder/my-projects/naive-bayes-jonahtjandra/tests/test_train_images_length_5";
+    std::ifstream input_file(file_path);
+    if (input_file.is_open()) {
+        input_file >> train;
+        input_file.close();
+    }
+    SECTION("Make sure that it correctly parses the digits") {
+        REQUIRE(train.GetImages().at(1).GetDigit() == 3);
+    }
+
+    SECTION("Make sure that it correctly parses the images") {
+        REQUIRE(train.GetImages().at(1).GetImage().size() == 25);
+    }
+
+    SECTION("Make sure that it parses all the images") {
+        REQUIRE(train.GetImages().size() == 2);
+    }
+}
+
